c92aa8a2a0ced0d985cd66e6642b445ce7e60d97
Rick <rickvgorp@hotmail.com>
2016-09-29 18:43:56 +0200

    Update decode.py

--- Write-up-Crypt0P1xels/decode.py
+++ Write-up-Crypt0P1xels/decode.py
@@ -1,14 +1,11 @@
 from PIL import Image
 import random
 
-FLAG = "Unknown"
-
+# Open image file and convert to RGB-format
 img = Image.open('encrypted.png')
 img_pix = img.convert('RGB')
 
-x = random.randint(1,255)
-y = random.randint(1,255)
-
+# Get flag length and coordinates of next pixel with data
 flaglen = (img_pix.getpixel((0,0)))[0]
 print("Flag length: " + str(flaglen))
 x = img_pix.getpixel((0,0))[1]
@@ -16,16 +13,19 @@ y = img_pix.getpixel((0,0))[2]
 
 string = ""
 
+# Get character from first next pixel
 string += chr(img_pix.getpixel((x,y))[0])
 x1 = img_pix.getpixel((x,y))[1]
 y1 = img_pix.getpixel((x,y))[2]
 
+# For loop to get all characters from the pixels
 for i in range(0,flaglen):
 	string += chr(img_pix.getpixel((x1,y1))[0])
 	result = img_pix.getpixel((x1,y1))
 	x1 = result[1]
 	y1 = result[2]
 
+# Print pixeldata
 print("Data found: " + string)
 	
 
